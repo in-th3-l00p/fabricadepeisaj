210ed352442a7fa30bd83c941f20c793929c8480
in-th3-l00p <indianucode0303@gmail.com>
2024-02-23 17:24:55 +0200

    removed alpha on contact name

--- routes/web.php
+++ routes/web.php
@@ -23,14 +23,13 @@ Route::view("/inspiratie", "inspiration")->name("inspiration");
 Route::view("/contact", "contact")->name("contact");
 Route::post("/contact", function (Request $request) {
     $data = $request->validate([
-        "name" => "required|min:3|max:255|alpha",
+        "name" => "required|min:3|max:255",
         "email" => "required|email|max:255",
         "message" => "required|min:1|max:3000"
     ], [
         "name.required" => "Numele este obligatoriu",
         "name.min" => "Numele trebuie sa aiba minim 3 caractere",
         "name.max" => "Numele trebuie sa aiba maxim 255 caractere",
-        "name.alpha" => "Numele trebuie sa contina doar litere",
         "email.required" => "Emailul este obligatoriu",
         "email.email" => "Emailul nu este valid",
         "email.max" => "Emailul trebuie sa aiba maxim 255 caractere",
