c415bc1eddd95556c87a5324bb9cfd934799733f
in-th3-l00p <indianucode0303@gmail.com>
2024-02-23 18:56:48 +0200

    fixed footer standard bug

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -2,12 +2,12 @@
     <div class="flex justify-center mb-8">
         <h2 class="text-2xl text-right">{{ env("APP_NAME") }}</h2>
         <div class="min-h-full p-[1px] bg-white mx-4"></div>
-        <ul class="flex flex-col gap-2 list-disc">
-            <a href="{{ route("about") }}" class="text-white hover:underline">{{ __("Despre noi") }}</a>
-            <a href="{{ route("partners") }}" class="text-white hover:underline">{{ __("Parteneri") }}</a>
-            <a href="{{ route("inspiration") }}" class="text-white hover:underline">{{ __("Inspirație") }}</a>
-            <a href="{{ route("news.index") }}" class="text-white hover:underline">{{ __("Noutați") }}</a>
-            <a href="{{ route("contact") }}" class="text-white hover:underline">{{ __("Contact") }}</a>
+        <ul class="flex flex-col gap-2">
+            <li><a href="{{ route("about") }}" class="text-white hover:underline">{{ __("Despre noi") }}</a></li>
+            <li><a href="{{ route("partners") }}" class="text-white hover:underline">{{ __("Parteneri") }}</a></li>
+            <li><a href="{{ route("inspiration") }}" class="text-white hover:underline">{{ __("Inspirație") }}</a></li>
+            <li><a href="{{ route("news.index") }}" class="text-white hover:underline">{{ __("Noutați") }}</a></li>
+            <li><a href="{{ route("contact") }}" class="text-white hover:underline">{{ __("Contact") }}</a></li>
         </ul>
     </div>
 
